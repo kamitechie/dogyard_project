a529509b7401def58869db218e1a3b05d5e6a5fe
kamitechie <kamitechie@gmail.com>
2023-08-09 09:50:29 +0300

    Created html,css files. Retrieved info from database.

--- CRUD_data.py
+++ CRUD_data.py
@@ -1,12 +1,37 @@
 from sqlalchemy.orm import sessionmaker
-from dogyard_models import MedicalRecord, Dog, engine
+from dogyard_models import Dog, engine
+import base64
 
 Session = sessionmaker(bind=engine)
 session = Session()
 all_dogs = session.query(Dog).all()
-for dog in all_dogs:
-    print(dog.dogs_name, dog.birth_date)
 
-hospital_all = session.query(MedicalRecord).all()
-for record in hospital_all:
-    print(record.id, record.is_in_rut, record.is_neutered, record.date_of_death)
+# Generate HTML content
+html_content = ""
+for dog in all_dogs:
+    image_bytes = dog.photo
+    image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+    html_content += f"""
+    <div class="dog">
+        <img src="data:image/jpeg;base64,{image_base64}" alt="{dog.dogs_name}">
+        <h2>{dog.dogs_name}</h2>
+        <p>{dog.birth_date}</p>
+        <p>{dog.pool}</p>
+    </div>
+    """
+
+# Insert the generated HTML content into the index.html file
+with open('index.html', 'w') as html_file:
+    html_file.write(f"""<!DOCTYPE html>
+    <html>
+    <head>
+        <title>Dogyard Dogs</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <h1>All dogs</h1>
+        <div class="Dogs">
+            {html_content}
+        </div>
+    </body>
+    </html>""")
